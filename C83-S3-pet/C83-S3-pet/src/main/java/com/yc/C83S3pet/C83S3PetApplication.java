0a72d777c57de60f42ed36b77b35be9d1d7dbd7c
sunzhuguo <1694472882@qq.com>
2020-09-24 15:36:37 +0800

    宠物商店初始化工程

---
+++ C83-S3-pet/C83-S3-pet/src/main/java/com/yc/C83S3pet/C83S3PetApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C83S3pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C83S3PetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C83S3PetApplication.class, args);
+	}
+
+}
---
+++ C83-S3-pet/C83-S3-pet/src/main/java/com/yc/C83S3pet/bean/pet.java
@@ -0,0 +1,11 @@
+package com.yc.C83S3pet.bean;
+
+public class pet implements java.io.Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+
+}
